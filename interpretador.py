63bd1b0401f1e1b3df9ce8639532840abdb20a51
heduardabolini <79718218+heduardabolini@users.noreply.github.com>
2023-10-31 11:18:27 -0300

    Add files via upload

--- interpretador.py
+++ interpretador.py
@@ -18,8 +18,7 @@ def guardarPosicao(exemplo):
 #controle de variaveis (lista ou tuplas olhar pela operacao)
 def listaExecucao(lista, posicao):
     execucao = {}
-    i = 0  #posição atual na lista
-    valida = False
+    i = 0
 
     while i < len(lista):
         tupla = lista[i]
@@ -29,39 +28,43 @@ def listaExecucao(lista, posicao):
                 print(tupla[2])
                 entrada = input()  
                 execucao[tupla[3]] = entrada
-               
-                for elemento in execucao:
-                    if elemento == tupla[3]:
-                        execucao[elemento] = entrada                                     
+                                              
             elif tupla[1] == 'print':
-                print(tupla[2])
+                if tupla[2] in execucao:
+                    print(execucao[tupla[2]])
+                else:
+                    print(tupla[2])
                 
         if tupla[0] in ['+', '-', '*', '/', '&&', '||', '=', '!', '==', '!=', '<', '<=', '>', '>=']:
-            for elemento in execucao:
-                if elemento == tupla[2]: #Duvida -> se a variavel nao existir?
-                    val = list(execucao.values())[0] + tupla[0] + tupla[3]
-                    valida = True  
-            if valida == False:
+            if tupla[2] in execucao:
+                op1 = execucao[tupla[2]]
+            else:
+                op1 = tupla[2]
+
+            if tupla[3] in execucao:
+                op2 = execucao[tupla[3]]
+            else:
+                op2 = tupla[3]
+
+            if op1.isnumeric() and op2.isnumeric():
+                val = eval(op1 + tupla[0] + op2)
+                execucao[tupla[1]] = val
+            else:
                 print("Variável não encontrada")
                 exit()
-            execucao[tupla[1]] = val 
-
-        if tupla[0] == 'if':
-            for elemento in execucao:
-                if elemento == tupla[1]:
-                    condicao = eval(execucao[tupla[1]])
-                    if condicao:
-                        label_pos = posicao[tupla[2]] 
-                        #print(label_pos)
-                        i = label_pos
-                    else:
-                        label_pos = posicao[tupla[3]]
-                        #print(label_pos)
-                        i = label_pos  
         
+        if tupla[0] == 'if':
+            if tupla[1] in execucao:
+                condicao = execucao[tupla[1]]
+                if condicao:
+                    label_pos = posicao[tupla[2]] 
+                    i = label_pos
+                else:
+                    label_pos = posicao[tupla[3]]
+                    i = label_pos  
+
         if tupla[0] == 'jump':
             label_pos = posicao[tupla[1]]
-            #print(label_pos)
             i = label_pos 
 
         i += 1 
